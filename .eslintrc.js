4a7209fb633f6349fc129010b38dc69c70740c5a
matheuscas <matheus.mcas@gmail.com>
2022-01-18 21:20:25 -0300

    Improve eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,24 @@
 module.exports = {
-  extends: 'eslint:recommended',
-  rules: {
-    'react/jsx-filename-extension': 'off',
-    //You can override any rules you want
-  },
   env: {
     browser: true,
-    es6: true,
-    jest: true,
+    es2021: true,
     node: true,
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
 };

bda82c7a4004aa8b5e571810efb76cf93d85842f
matheuscas <matheus.mcas@gmail.com>
2022-01-19 14:15:51 -0300

    Fix prettier issues

--- src/services/items.tsx
+++ src/services/items.tsx
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
 export interface Item {
-    category: number
-    id: number
-    image_id: string
-    name: string
-    price: string
+  category: number;
+  id: number;
+  image_id: string;
+  name: string;
+  price: string;
 }
 
 async function stall(stallTime = 3000) {
-    await new Promise(resolve => {
-        setTimeout(resolve, stallTime);
-    });
+  await new Promise((resolve) => {
+    setTimeout(resolve, stallTime);
+  });
 }
 
-const getItems = async () :Promise<Item[]> => {
-    await stall()
-    const response = await axios.get<Item[]>('http://localhost:8000/api/v1/menu')
-    return response.data
-}
+const getItems = async (): Promise<Item[]> => {
+  await stall();
+  const response = await axios.get<Item[]>('http://localhost:8000/api/v1/menu');
+  return response.data;
+};
 
-export default getItems
+export default getItems;
